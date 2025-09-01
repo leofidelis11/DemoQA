109df581f0e8d85325a9fb75669c2448f3b6b6a2
Leonardo Fidelis <leofidelis11@hotmail.com>
2025-09-01 01:58:39 -0300

    removind unecessary fields in forms

--- cypress/e2e/challengePart2.cy.js
+++ cypress/e2e/challengePart2.cy.js
@@ -16,10 +16,10 @@ describe('Challenge part 2', () => {
     cy.get('#userEmail').type('mickeymouse@example.com');
     cy.get('label[for="gender-radio-1"]').contains('Male').click();
     cy.get('#userNumber').type('1234567891');
-    cy.get('#subjectsInput').type('Maths');
-    cy.get('#hobbies-checkbox-1').check();
+    //cy.get('#subjectsInput').type('Maths');
+    //cy.get('#hobbies-checkbox-1').check();
     cy.get('#uploadPicture').selectFile('sampleFileToUpload.txt');
-    cy.get('#currentAddress').type("Disney's Hollywood Studios - Orlando, Florida USA");
+    //cy.get('#currentAddress').type("Disney's Hollywood Studios - Orlando, Florida USA");
     cy.get('#submit').click();
 
     cy.get('#example-modal-sizes-title-lg').contains('Thanks').should('be.visible')
